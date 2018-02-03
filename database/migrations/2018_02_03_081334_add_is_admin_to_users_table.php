3c10e226f69cebd9944853e0ff3127a8e5db4c31
zzjdy_0 <2633203405@qq.com>
2018-02-03 18:38:38 +0000

    nima

--- database/migrations/2018_02_03_081334_add_is_admin_to_users_table.php
+++ database/migrations/2018_02_03_081334_add_is_admin_to_users_table.php
@@ -14,7 +14,7 @@ class AddIsAdminToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_admin')->defualt(false);
+            $table->boolean('is_admin')->default(false);
         });
     }
 
